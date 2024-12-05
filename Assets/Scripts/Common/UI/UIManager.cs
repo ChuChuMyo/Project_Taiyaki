95aa8768cd17ab05b6a8c39ef1085c9f9ed0fa33
ChuChuMyo <ben0310@naver.com>
2024-12-05 16:50:46 +0900

    0.0.7

--- Assets/Scripts/Common/UI/UIManager.cs
+++ Assets/Scripts/Common/UI/UIManager.cs
@@ -3,10 +3,13 @@ using System.Collections.Generic;
 using UnityEngine;
 using Sirenix.OdinInspector;
 using TMPro;
-using DG.Tweening;
 
 public class UIManager : Singleton<UIManager>
 {
+    public Transform UICanvasTrs;
+    public Transform m_ClosedUITrs;
+    //public Transform ClosedUITrs;
+
     protected override void Init()
     {
         base.Init();
